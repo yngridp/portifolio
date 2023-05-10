5fb3e151ce80ae61c35d539fb17ea48d349c0daf
Yngrid Padilha <yngrid_padilha@hotmail.com>
2023-05-10 15:43:03 -0300

    Criação do JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,127 @@
+﻿
+//Variaveis do escopo global
+
+const nome = document.querySelector("#nome"); /*pegando o input, armazenando na constante nome o que tem dentro da div, query selector -consultar seletor*/ 
+const email = document.querySelector("#email");
+const mensagem = document.querySelector("#mensagem");
+
+let nomeOk = false;
+let emailOk = false;
+let mensagemOk = false;
+let cepOk = false;
+
+function validarNome() {
+
+    //variavel do escoo local
+
+  let txtNome = document.querySelector("#txtNome"); //recebendo em txt Nome o que está dentro da div txtNome
+
+  if (nome.value.length < 3) {
+    txtNome.innerHTML = "Nome muito curto";
+    txtNome.style.color = "red";
+    nomeOk = false;
+  } else {
+    txtNome.innerHTML = "✔";
+    txtNome.style.color = "green";
+    nomeOk = true;
+  }
+}
+
+function validarEmail() {
+  let txtEmail = document.querySelector("#txtEmail");
+
+  if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+    txtEmail.innerHTML = "E-mail inválido";
+    txtEmail.style.color = "red";
+    emailOk = false;
+  } else {
+    txtEmail.innerHTML = "✔";
+    txtEmail.style.color = "green";
+    emailOk = true;
+  }
+}
+
+function validarEmail2() { //expressão regular para validar email
+  let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  let txtEmail = document.querySelector("#txtEmail");
+
+  if (email.value.match(regex)) {
+    txtEmail.innerHTML = "✔";
+    txtEmail.style.color = "green";
+    emailOk = true;
+  } else {
+    txtEmail.innerHTML = "E-mail inválido";
+    txtEmail.style.color = "red";
+    emailOk = false;
+  }
+}
+
+function validarMensagem() {
+  let txtMensagem = document.querySelector("#txtMensagem");
+
+  if (mensagem.value.length >= 100) {
+    txtMensagem.innerHTML = "Mensagem muito grande!";
+    txtMensagem.style.color = "red";
+    mensagemOk = false;
+  } else {
+    txtMensagem.innerHTML = "✔";
+    txtMensagem.style.color = "green";
+    mensagemOk = true;
+  }
+}
+
+function enviarForm() {
+  if (nomeOk === true && emailOk === true && mensagemOk === true) {
+    alert(nome.value + ", obrigada pela sua mensagem!");
+  } else {
+    alert("Por favor, preencha todos os campos corretamente.");
+  }
+}
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+function validarCep() {
+  const cep = document.getElementById("cep").value.replace("-", "");
+
+  if (cepValido(cep)) {
+    txtCep.innerHTML = "✔";
+    txtCep.style.color = "green";
+    cepOk = true;
+  } else {
+    txtCep.innerHTML = "CEP Inválido";
+    txtCep.style.color = "red";
+    cepOK = false;
+  }
+}
+
+function consultarCep() {
+  document.getElementById("dados").innerHTML = "";
+console.log(cepOk);
+const cep = document.getElementById("cep").value.replace("-", "");
+const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+if (cepOk === true || cepValido(cep)) {
+   
+    fetch(url)
+      .then((response) => response.json())
+      .then((jsonBody) => {
+        if (jsonBody.erro === true || jsonBody.logradouro === undefined) {
+          alert("CEP nao encontrado!");
+        } else {
+          document.getElementById("dados").innerHTML =
+            jsonBody.logradouro +
+            "\n" +
+            jsonBody.bairro +
+            "\n" +
+            jsonBody.localidade +
+            "\n" +
+            jsonBody.uf;
+        }
+      })
+      .catch((error) => {
+        alert("CEP não encontrado!");
+      });
+  }
+}
---
+++ teste.js
@@ -0,0 +1,49 @@
+//console.log("Hello World!")
+
+let nome = "Yngrid" // let posso mudar a variavel dps
+
+const sobrenome = "Padilha" //const nao da pra mudar valor
+
+console.log(nome + " " +sobrenome)
+
+nome = "Mariana"
+
+console.log(nome) /*console para mudar nome da variavel*/
+
+let numero1 = 30
+let numero2 = 10
+
+console.log(numero1 + numero2) /*console.log igual a println no java*/
+
+function dividir(num1, num2){ 
+
+    if(num2 === 0){ /* operador de comparação 3x = */
+       return "Não é possível dividir por zero"
+    }
+
+    return num1 / num2
+}
+
+let resultado = dividir(10,5)
+console.log(resultado)
+// uma variavel pode receber com constante com valor
+//se eu quiser mudar o valor da variavel usar let
+const soma = (num1, num2) => num1 + num2 /*forma simplificada de uma função*/
+
+console.log(soma(20,30))
+
+function tabuada(num1){
+    for(let contador = 1; contador <= 10; contador ++){
+        console.log(`${num1} x ${contador} = ${num1 * contador}` )
+    }
+}
+
+tabuada(5)
+
+/*criar um vetor*/
+
+const nomes = ["Yngrid", "Stéfani", "Maria", "Julia"]
+
+nomes.forEach(nome => console.log(nome)) /*percorrendo o vetor e cada posição que achar vai guardar na variavel nome e imprimir na tela*/
+
+
